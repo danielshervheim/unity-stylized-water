f871aabee302491dd6fc9c4683dbe62214c2146f
Daniel Shervheim <danielshervheim@gmail.com>
2020-02-11 00:10:50 -0600

    Added depth buffer enabler script.

---
+++ Assets/Stylized Water/EnableDepthBuffer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class EnableDepthBuffer : MonoBehaviour
+{
+    private Camera m_camera;
+
+    void Update()
+    {
+        if (m_camera == null)
+        {
+            m_camera = GetComponent<Camera>();
+        }
+
+        if (m_camera.depthTextureMode == DepthTextureMode.None)
+        {
+            m_camera.depthTextureMode = DepthTextureMode.Depth;
+        }
+    }
+}
